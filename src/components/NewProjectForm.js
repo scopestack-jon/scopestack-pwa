7e73cc05b177924d83e3c01993ea3d1b6b217289
scopestack-jon <jon@scopestack.io>
2025-02-19 20:44:33 -0500

    Update logo image source to GitHub raw content URL

--- src/components/NewProjectForm.js
+++ src/components/NewProjectForm.js
@@ -318,7 +318,7 @@ const NewProjectForm = () => {
           </div>
         </div>
         <img 
-          src="/assets/SS-Logo-Horiztonal-allwhite.png"
+          src="https://github.com/scopestack-jon/scopestack-pwa/blob/main/public/assets/SS-Logo-Horiztonal-allwhite.png"
           alt="ScopeStack" 
           className="header-logo"
         />
